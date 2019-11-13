adaa1ae80e386b12b2a680cf0f53ece4939515e4
Güttinger Luca <luca.guettinger@cmiag.ch>
2019-11-13 16:47:52 +0100

    write small basic concept and rename some properties

--- src/main/java/com/github/modul226b/BusManager/model/BusType.java
+++ src/main/java/com/github/modul226b/BusManager/model/BusType.java
@@ -1,5 +1,10 @@
 package com.github.modul226b.BusManager.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class BusType {
     String name;
     int capacity;
