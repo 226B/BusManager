ebb442cd596bfbc9180482cc4157ed95cd866898
McTNTCrafter // luca <luca.guettinger@gmail.com>
2020-01-15 22:30:48 +0100

    write failing unit test

--- src/main/java/com/github/modul226b/BusManager/validator/internal/ValidationManager.java
+++ src/main/java/com/github/modul226b/BusManager/validator/internal/ValidationManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class ValidationManager{
+public class            ValidationManager{
     private static ValidationManager instance;
 
     public static ValidationManager getInstance() {
@@ -39,7 +39,20 @@ public class ValidationManager{
     }
 
     public List<ValidationResult> validate() {
+        List<ValidationResult> results = new ArrayList<>();
+        for (Validator<?> validator : validatorList) {
+            this.validate(results, validator);
+        }
+        return results;
+    }
+
+    private void validate(List<ValidationResult> results, Validator<?> validator) {
         //todo
-        return null;
+    }
+
+    public List<ValidationResult> validate(Validator<?> validator) {
+        ArrayList<ValidationResult> results = new ArrayList<>();
+        this.validate(results, validator);
+        return results;
     }
 }
--- src/test/java/com/github/modul226b/BusManager/ValidationTests.java
+++ src/test/java/com/github/modul226b/BusManager/ValidationTests.java
@@ -7,6 +7,7 @@ import com.github.modul226b.BusManager.model.Location;
 import com.github.modul226b.BusManager.validator.internal.ValidationManager;
 import com.github.modul226b.BusManager.validator.internal.ValidationResult;
 import com.github.modul226b.BusManager.validator.internal.ValidationState;
+import com.github.modul226b.BusManager.validator.validators.NullValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +21,7 @@ public class ValidationTests {
 
         DataManager.getInstance().addLocation(new Location(100, 100));
 
-        List<ValidationResult> result = manager.validate();
+        List<ValidationResult> result = manager.validate(new NullValidator());
 
         Assertions.assertEquals(1, result.size());
         Assertions.assertEquals(ValidationState.SUCCESS, result.get(0).getValidation());
